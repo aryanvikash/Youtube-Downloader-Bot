218d5818a266ab367bd79e0e03ba15090cb7f328
jahongirvlogs <63680003+jahongirvlogs@users.noreply.github.com>
2020-12-09 19:25:17 +0500

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,10 +5,12 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel", url="https://t.me/aryan_bots")],
+        [InlineKeyboardButton("🌐 WebSite", url="https://ganiyev.uz")],
         [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/aryanvikash")]
+            "Admin 😊", url="https://t.me/jokkerking")]
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    welcomed = f"Salom <b>{message.from_user.first_name}</b>\nTelegramdagi eng zamonaviy YouTube video va audio yuklovchi botga xush kelibsiz!
+
+Iltimos, har qanday YouTube video havolasini yuboring yoki @vid inline rejimidan foydalanib qidiring."
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
