e1c2fde13f5fb8f58b2e4c30f007df151847be84
Vivektp <75740863+Vivektp@users.noreply.github.com>
2020-12-28 20:48:01 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -9,6 +9,6 @@ async def start(client, message):
         [InlineKeyboardButton(
             "Report Bugs 😊", url="https://t.me/aryanvikash")]
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/**Join On @VKPROJECTS** __NOW Sent A YouTube Video Url....__  /help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
