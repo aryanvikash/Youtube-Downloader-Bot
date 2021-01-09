37064193fe4c79eee1401e2a3d36e13e8281279f
fazinek <76585579+fazinek@users.noreply.github.com>
2021-01-09 09:42:52 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,10 +5,11 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel", url="https://t.me/aryan_bots")],
+        [InlineKeyboardButton("Channel", url="https://t.me/ekbotzupdate")],
         [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/aryanvikash")]
+            "Report Bugs 😊", url="https://t.me/Ek_comme_nt_bot")]
     ])
+        [InlineKeyboardButton("Share, url="https://t.me/share/url?url=@ytb_dl_ek_bot")]
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
