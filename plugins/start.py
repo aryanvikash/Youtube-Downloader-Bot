ab722fb22552b0066213b4c58953057c0915f0f4
123456789592 <77098458+123456789592@users.noreply.github.com>
2021-01-16 15:33:51 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,10 +5,10 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel", url="https://t.me/aryan_bots")],
+        [InlineKeyboardButton("Channel", url="https://t.me/Mega_Bots_Updates")],
         [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/aryanvikash")]
+            "Report Bugs 😊", url="https://t.me/Mega_Bots_Supporters")]
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\nYou Must join my updation channel⬇️⬇️/help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
