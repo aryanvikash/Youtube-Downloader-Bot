fe291bcbe47862053e815d5ec91846e1f398769d
mastersirrey <82076213+mastersirrey@users.noreply.github.com>
2021-04-16 07:37:01 +0000

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url"
+    helptxt = f"cukup kirimkan saja link video youtube yang ingin di download"
     await message.reply_text(helptxt)
