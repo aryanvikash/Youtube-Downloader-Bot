5c0a78b134eeabdbdd7b95faca6e1abc2d556f3c
PokemModder <83958349+PokemModder@users.noreply.github.com>
2021-05-10 23:19:37 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -7,7 +7,7 @@ async def start(client, message):
     joinButton = InlineKeyboardMarkup([
         [InlineKeyboardButton("Channel", url="https://t.me/aryan_bots")],
         [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/aryanvikash")]
+            "Report Bugs 😊", url="https://t.me/PokemonModder_Owner")]
     ])
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
