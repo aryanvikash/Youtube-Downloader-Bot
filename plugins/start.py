0d4fb871005ba41fb15a4e4c3f482612cb5eb14b
Sanila Ranatunga <88930336+sanila2007@users.noreply.github.com>
2021-09-19 07:10:04 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -9,6 +9,7 @@ async def start(client, message):
         [InlineKeyboardButton(
             "Report Bugs 😊", url="https://t.me/aryanvikash")]
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info.
+    (Created by @SanilaRanatunga)"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
