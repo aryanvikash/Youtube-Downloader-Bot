faa08619aa622e3fdd49a0cb7ec474a6b2d4e297
royfds <74107306+royfds@users.noreply.github.com>
2021-11-12 12:26:35 +0530

    start.py

--- MyTestBotZ/plugins/start.py
+++ MyTestBotZ/plugins/start.py
@@ -5,12 +5,12 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel", url="https://t.me/MyTestBotz"), InlineKeyboardButton("Creator", url="https://telegram.me/OO7ROBot") ],
+        [InlineKeyboardButton("Channel", url="https://t.me/MyTestBotz"), InlineKeyboardButton("Creator", url="https://telegram.me/Moviezbarn") ],
         [InlineKeyboardButton(
             "🍿 Source Code 🍿", url="https://github.com/")]
     ])
     welcomed = f"""Hey <b>{message.from_user.first_name}</b>\nA Simple YouTube Downloader Bot that can:
   ➠ Download YouTube videos
-  ➠ Download audio from YouTube videos \n\n Made with ♥️ by @MyTestBotZ"""
+  ➠ Download audio from YouTube videos \n\n Made with ♥️ by @moviezbarn"""
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
